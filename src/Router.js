d05e7be72f73c717c6b54389a4739de4292c799f
rodolfomori <rod_dolpho@hotmail.com>
2022-10-05 15:29:57 -0300

    devclub-webview

---
+++ src/Router.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Home } from './containers/Home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}>
+      </Route>
+    </Routes>
+  );
+}
+
