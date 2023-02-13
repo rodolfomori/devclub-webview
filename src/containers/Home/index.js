6b433bfd18d4ea4568f64f8c62d7ceab6fc39ed3
rodolfomori <rod_dolpho@hotmail.com>
2023-02-12 21:20:51 -0300

    tirando acesso de 2 anos

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,11 +2,10 @@
 // import { Iframe} from './styles'
 // import { useHome } from '../../hooks/HomeContext'
 // import HashLoader from 'react-spinners/HashLoader'
-// import { ContentBanners } from '../ContentBanners'
-import { Launch } from '../Launch'
+import { ContentBanners } from '../ContentBanners'
 
 
 export function Home() {
 
-  return <Launch />
+  return <ContentBanners />
 }
