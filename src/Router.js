39ea94ea6d776707069bc3384dd98f2b1b149559
rodolfomori <rod_dolpho@hotmail.com>
2023-03-26 16:27:51 -0300

    aula 18

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,13 @@
 import React from 'react'
 
-
-
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
 
 import { Home } from './containers/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-      </Route>
+      <Route path="/" element={<Home />}></Route>
     </Routes>
-  );
+  )
 }
-
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,11 +2,11 @@
 // import { Iframe} from './styles'
 // import { useHome } from '../../hooks/HomeContext'
 // import HashLoader from 'react-spinners/HashLoader'
-// import { ContentBanners } from '../ContentBanners'
-import { Launch } from '../Launch'
+import { ContentBanners } from '../ContentBanners'
+// import { Launch } from '../Launch'
 
 
 export function Home() {
 
-  return <Launch />
+  return <ContentBanners />
 }
