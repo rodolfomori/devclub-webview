ea18aad248a1992a1a4880a91d8820d17ad5a4a3
rodolfomori <rod_dolpho@hotmail.com>
2023-07-11 09:03:02 -0300

    add event

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,17 @@
 // import { BottomMenu } from '../../components/BottomMenu'
-// import { Iframe} from './styles'
+import { Iframe} from './styles'
 // import { useHome } from '../../hooks/HomeContext'
 // import HashLoader from 'react-spinners/HashLoader'
-import { ContentBanners } from '../ContentBanners'
+// import { ContentBanners } from '../ContentBanners'
 // import { Launch } from '../Launch'
 
 
 export function Home() {
 
-  return <ContentBanners />
+  // return <ContentBanners />
+  return (
+    <div>
+    <Iframe src="https://rodolfomori.com.br/missao" height="100vh" width="100vw"></Iframe>
+    </div>
+  )
 }
