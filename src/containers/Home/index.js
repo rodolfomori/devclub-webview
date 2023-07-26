6560cca4e32a83d10d4c7e95f03fd8332647f1a1
rodolfomori <rod_dolpho@hotmail.com>
2023-07-26 13:26:38 -0300

    home original

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,17 +1,19 @@
 // import { BottomMenu } from '../../components/BottomMenu'
-import { Iframe} from './styles'
+// import { Iframe } from './styles'
 // import { useHome } from '../../hooks/HomeContext'
 // import HashLoader from 'react-spinners/HashLoader'
-// import { ContentBanners } from '../ContentBanners'
+import { ContentBanners } from '../ContentBanners'
 // import { Launch } from '../Launch'
 
-
 export function Home() {
-
-  // return <ContentBanners />
-  return (
-    <div>
-    <Iframe src="https://rodolfomori.com.br/missao" height="100vh" width="100vw"></Iframe>
-    </div>
-  )
+  return <ContentBanners />
+  // return (
+  //   <div>
+  //     <Iframe
+  //       src="https://rodolfomori.com.br/missao"
+  //       height="100vh"
+  //       width="100vw"
+  //     ></Iframe>
+  //   </div>
+  // )
 }
